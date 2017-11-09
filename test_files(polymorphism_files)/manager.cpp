85c15d0a6a8a3720db1600ae87846f7858398010
Henry Bang <henrybang09@gmail.com>
2017-11-08 22:57:46 -0600

    added test files for operator overload and create_order.png

--- test_files(polymorphism_files)/manager.cpp
+++
@@ -1,7 +0,0 @@
-#include "manager.h"
-
-
-
-Manager::Manager(string name, int id, string phone) :Person(name,id, phone) {}
-
-string Manager::type(){return "Manager";}
--- test_files(polymorphism_files)/person.cpp
+++
@@ -1,5 +0,0 @@
-#include "person.h"
-
-
-Person::Person(string name, int id, string phone) : _name{name}, _id{id}, _phone{phone} {}
-string Person::type(){return "Person";}
